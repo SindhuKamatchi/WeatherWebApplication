7b0840bb40f228b1d524e69c155a268e23e415bf
SindhuKamatchi <sindhukamatchi@gmail.com>
2022-09-03 23:40:59 +0100

    Add files via upload

---
+++ WeatherForecastTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("WeatherForecastTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Santander UK")]
+[assembly: AssemblyProduct("WeatherForecastTest")]
+[assembly: AssemblyCopyright("Copyright © Santander UK 2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("bbfef280-12a8-4fbb-9b1e-4a7d85b82376")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ WeatherForecastTest/UnitTest.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using WeatherForecase;
+
+namespace WeatherForecastTest
+{
+    [TestClass]
+    public class UnitTest
+    {
+       const string Success= "Successfully Logged";
+       const  string Failure= "Logging failed";
+       const string exception = "Logging failed due to exception";
+
+        [TestMethod]
+        public void ValidRequestandResponse()
+        {
+            string request = "Request";
+            string response = "Response";
+            string result = WeatherForecase.Forecast.Logging(request, response);
+            Assert.AreEqual(string.Compare(result, Success), 0);
+        }
+
+        [TestMethod]
+        public  void emptyRequestandResponse()
+        {
+            string request = string.Empty;
+            string response = string.Empty;
+            string result=WeatherForecase.Forecast.Logging(request, response);
+            Assert.AreEqual(string.Compare(result,Failure),0);
+        }
+    }
+}
