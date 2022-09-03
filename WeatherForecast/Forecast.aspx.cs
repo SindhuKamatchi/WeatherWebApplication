644a70f2d1c1957e991c17c6736fddd7ab19e655
SindhuKamatchi <sindhukamatchi@gmail.com>
2022-09-03 23:38:37 +0100

    Add files via upload

---
+++ WeatherForecast/Forecast.aspx.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WeatherForecase
+{
+    public partial class Forecast : System.Web.UI.Page
+    {
+        /// <summary>
+        /// Class <c>Logging</c> To log the reques and response.
+        /// </summary>
+        [WebMethod]
+        public static string Logging(string request,string response)
+        {
+            string result = string.Empty;
+            try
+            {
+                if (!string.IsNullOrWhiteSpace(request) && !string.IsNullOrWhiteSpace(response))
+                {
+                    using (StreamWriter writetext = File.AppendText("Logs.txt"))
+                    {
+                        writetext.WriteLine("Date: " + System.DateTime.Now + " Request:" + request + " Response:" + response);
+                    }
+                    result = "Successfully Logged";
+                }
+                else
+                {
+                    result = "Logging failed";
+                }
+            }
+           catch(Exception ex)
+            {
+                result = "Logging failed due to exception";
+                Debug.WriteLine(ex);
+            }
+            return result;
+            
+        }
+    }
+}
